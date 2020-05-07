6157dc6d734934d44ef27dfb3273c817a75478f2
Shawn Gluchacki <Shawn@MacBook-Pro-2.local>
2020-05-07 13:43:37 -0600

    Address all user stories, functionality complete

--- js/main.js
+++ js/main.js
@@ -21,20 +21,25 @@ $inputField.keypress(function(evt) {
     }
 });
 
-$('.remove-skill').click(function(evt) {
-    console.log('clicked on the X');
-    console.log(evt.target);
-})
 
 // console.log($inputField);
 
 function addSkill(evt) {
     let currentInput;
     let $li = $("<li>");
+    let idCurrent = `${Date.now()}`    //generates a unique id, since we can't enter more than one skill per millisecond
     console.log(evt);
     currentInput = $inputField.val();
     console.log(`Current input: ${currentInput}`);
     $inputField.val(null);
-    $li.html(`<span class="remove-skill">X</span>${currentInput}`);
+    $li.html(`<span class='remove-skill'>X</span>${currentInput}`);
+    $li.find('.remove-skill').attr('id', idCurrent);
     $('ul').append($li);
+    $(`#${idCurrent}`).click(function(evt) {
+        // console.log('clicked on the X');
+        // console.log(`id = ${idCurrent}`);
+        // console.log(evt.target);
+        // console.log($(`#${idCurrent}`).parent());
+       $(`#${idCurrent}`).parent().remove();
+    });
 }
